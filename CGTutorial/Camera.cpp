a01e02b435b7968e26311157c40ae4e8c4b10169
Eric Vuglar <s171764@students.aie.edu.au>
2018-05-15 09:50:11 +0930

    Added skeleton of Camera class

---
+++ CGTutorial/Camera.cpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "Camera.h"
+
+Camera::Camera()
+{
+
+}
+
+Camera::~Camera()
+{
+
+}
+
+void Camera::setPerspective(float fieldOfView, float aspectRatio, float near, float far)
+{
+
+}
+
+void Camera::setLookAt(vec3 from, vec3 to, vec3 up)
+{
+
+}
+
+void Camera::setPosition(vec3 position)
+{
+
+}
+
+void Camera::updateProjectionViewTransform()
+{
+
+}
---
+++ CGTutorial/Camera.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <glm\glm.hpp>
+#include <glm\ext.hpp>
+
+using glm::mat4;
+using glm::vec3;
+
+class Camera
+{
+public:
+	Camera();
+	~Camera();
+
+	virtual void update(float deltaTime) = 0;
+
+	void setPerspective(float fieldOfView, float aspectRatio, float near, float far);
+	void setLookAt(vec3 from, vec3 to, vec3 up);
+	void setPosition(vec3 position);
+
+	mat4 getWorldTransform() { return worldTransform; }
+	mat4 getView() { return viewTransform; }
+	mat4 getProjection() { return projectionTransform; }
+	mat4 getProjectionView() { return projectionViewTransform; }
+
+private:
+	mat4 worldTransform, viewTransform, projectionTransform, projectionViewTransform;
+
+	void updateProjectionViewTransform();
+};
